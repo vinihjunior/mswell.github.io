dde3022d42a303dced734c6114b2d3b794ceeadd
Wellington Moraes <wellpunk@gmail.com>
2016-08-02 16:47:11 -0300

    init branch pelican

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Wellington Moraes'
+SITENAME = u'Meu C\xf3digo'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = u'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Theme
+THEME = './pelican-bold'
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/wellpunk'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
